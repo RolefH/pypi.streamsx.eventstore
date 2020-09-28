826244c341d9b160ffdd524377b8ae001919226c
markheger <mark-oliver.heger@de.ibm.com>
2020-09-28 14:02:34 +0000

    python3.7 added

--- package/setup.py
+++ package/setup.py
@@ -18,6 +18,7 @@ setup(
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
   install_requires=['streamsx>=1.14.6,<2.0','streamsx.toolkits','streamsx.database>=1.5.0'],
   
